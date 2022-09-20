e3c2960b966d871ddb9504965f46497f63fce0ab
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-09-20 22:08:50 +0300

    Add files via upload

---
+++ Lecture3/ControlTest.java
@@ -0,0 +1,25 @@
+package Lecture.Lecture3;
+
+import java.text.DecimalFormat;
+
+public class ControlTest {
+    public static void main(String[] args) {
+        int demand = 100;
+        int suply = 50;
+        int price = 10;
+        double a = 0.1;
+        double b = 0.2;
+        DecimalFormat df = new DecimalFormat("0.00");// specifies the number of decimal places
+        
+
+        if (demand > suply){
+            Double increase = price + a*(demand-suply);//raises the price 
+            System.out.println("the current price :"+ df.format(increase));
+
+        }
+        else if (demand < suply){
+            Double decrease = price - b*(suply-demand);// lowers the price
+            System.out.println("the current price :"+ df.format(decrease));
+        }
+    }
+}
---
+++ Lecture3/Velocity.java
@@ -0,0 +1,20 @@
+package Lecture.Lecture3;
+import javax.swing.*;
+public class Velocity {
+    public static void main(String[] args) {
+        //compting train velocities from Entebbe to jinja
+        String input = JOptionPane.showInputDialog("Enter time");
+        int time = Integer.parseInt(input);
+        Double distance = 225.000;
+        distance = distance - 25;
+        // considering acceration to be zero
+        Double velocity = (distance/time);
+        System.out.println(velocity>60);//does not need conditionl statemets to provide boolean values.
+        time -=1;
+        System.out.println(velocity>60 && distance<225);
+        System.out.println(velocity>70 || distance<175 || time <= 3);
+
+
+    }
+    
+}

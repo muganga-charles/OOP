dab8e563450503975643d504be49eab8b932c839
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-09-24 23:06:08 +0300

    Update InverseTest.java

--- Lecture5/InverseTest.java
+++ Lecture5/InverseTest.java
@@ -6,7 +6,7 @@ public class InverseTest {
         double deltax = 0.1;
         final double TOLERANCE = 1E-14;
         for(double x = xStart; x < xEnd;x += deltax ){
-            //System.out.println(x);
+            //System
             double calc =1/(xEnd-x );
             System.out.println(calc);
         }

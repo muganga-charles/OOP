4829ef3279453d087ac0e02b9e58848ad2af6195
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-09-28 23:20:42 +0300

    Update MethodExercise.java

--- Lecture6/MethodExercise.java
+++ Lecture6/MethodExercise.java
@@ -23,7 +23,7 @@ public class MethodExercise {
         }
         return false;
     }
-    public static void printInfo(String name,int age, double height){
+    public static void printInfo(String name,int age, double height){ // prints
         System.out.println("The name is "+ name);
         System.out.println("The height is "+ height);
         System.out.println("The age is "+ age);

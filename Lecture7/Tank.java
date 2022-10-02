8651acfeac3cc83b419ee0f3c1f1cc24c7b850dc
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-10-02 21:29:38 +0300

    Add files via upload

---
+++ Lecture7/Tank.java
@@ -0,0 +1,48 @@
+package Lecture.Lecture7;
+
+public class Tank {
+    private double radius; 
+    private double length; 
+    private double thickness; 
+    
+    
+    // Constructor or existence method—public (no return type ever) 
+    public Tank(double r, double len, double t) { 
+        radius = r; 
+        length = len; 
+        thickness = t; 
+    } 
+    // Cylinder wall mass= Lπ((R+t)2 – R2) 
+    public double getMassCylinderWalls() { 
+        double mass= length*Math.PI*((radius + thickness)*(radius + thickness)- radius*radius);
+        return mass; 
+    } 
+    
+    // Cylinder end mass= 2πt(R+t)2 
+    public double getMassEnds() { 
+        double mass= 2.0*Math.PI*thickness* (radius + thickness)*(radius + thickness); 
+        return mass; 
+    } 
+    
+    public double getMass() { // We ignore density 
+        return getMassCylinderWalls() + getMassEnds(); 
+    } 
+    // volue  πR^2L
+    public double volume(){
+        return Math.PI*radius*radius*length;
+    }
+    // weld = 8π(R+t/2)
+    public double weld (){
+        return 8*Math.PI*(radius+(thickness/2));
+        //Ttm
+    }
+    public void setRadius (double r){
+        radius  = r;
+    }
+    public void setLength (double l){
+        length = l;
+    }
+    public void setThicknes (double t){
+        thickness  = t;
+    }
+}
---
+++ Lecture7/TankTest.java
@@ -0,0 +1,24 @@
+package Lecture.Lecture7;
+
+public class TankTest {
+    public static void main(String[] args) { 
+        // Create two Tank objects 
+        Tank t0= new Tank(0.5, 4.0, 0.04); 
+        Tank t1= new Tank(0.8, 1.5625, 0.04); 
+        
+        // Ask object t0 to tell you its mass 
+        double t0wall= t0.getMassCylinderWalls(); 
+        double t0end= t0.getMassEnds(); 
+        double t0mass= t0.getMass(); 
+        System.out.println("t0: "+ t0wall + " " + t0end + 
+        " " + t0mass); 
+        
+        // Ask object t1 to tell you its mass 
+        double t1wall= t1.getMassCylinderWalls(); 
+        double t1end= t1.getMassEnds(); 
+        double t1mass= t1.getMass(); 
+        System.out.println("t1: "+ t1wall + " " + t1end + 
+        " " + t1mass); 
+        }
+    
+}

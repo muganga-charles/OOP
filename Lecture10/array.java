c36ddbfddceba39d728def6f103ccdd84cbf0672
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-10-04 21:08:56 +0300

    Add files via upload

---
+++ Lecture10/array.java
@@ -0,0 +1,48 @@
+// package Lecture.Lecture10;
+
+// import java.util.Scanner;
+
+// public class array {
+//     public static void main(String[] args) {
+//         int [] intArray = new int[20];
+//         //int aSize = intArray.length; // aSize = 20
+//         //System.out.println(aSize);
+
+//         intArray [0] =4;
+//         intArray[1] = 77;
+//         //System.out.println(intArray[0]);
+//         array1();
+//     }
+//     //looping through
+//     public static void array1(){
+//         int [] arrayOne = new int[5];
+//         int lengthOfArray = arrayOne.length;
+//         Scanner inputt = new Scanner(System.in);
+//         for(int i= 0;i<lengthOfArray;i++){
+//             System.out.print("Enter element "+ (i)+ " : ");
+//             int x = inputt.nextInt();
+//             //addX(lengthOfArray,arrayOne,x);       
+       
+
+//         }
+//         System.out.println(arrayOne.toString());
+//     }
+// //     public static int[] addX(int n, int array[], int x)
+// //    {
+// //        int i;
+   
+// //        // create a new array of size n+1
+// //        int newarr[] = new int[n + 1];
+   
+// //        // insert the elements from
+// //        // the old array into the new array
+// //        // insert all elements till n
+// //        // then insert x at n+1
+// //        for (i = 0; i < n; i++)
+// //            newarr[i] = a[i];
+   
+// //        newarr[n] = x;
+   
+// //        return newarr;
+// //    }
+// }
---
+++ Lecture8/Collection.java
@@ -0,0 +1,16 @@
+package Lecture.Lecture8;
+import java.text.DecimalFormat;
+
+import Lecture.Lecture7.*;
+public class Collection {
+    public static void main(String[] args) {
+        double radius = 12.00;
+        double thickness = 32.12;
+        double height = 94.3;
+        float y =20f;
+        DecimalFormat df = new DecimalFormat("0.00");
+        Lecture.Lecture7.Tank tank1 = new Tank(radius, height, thickness);
+        System.out.println(y);
+        System.out.println("The mass is : " + df.format(tank1.getMass()));
+    }
+}
